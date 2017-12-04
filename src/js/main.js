ae1e32e028e3bfd9c2075ac4d96113d86246c9e2
¨keyhe.16@gmail.com¨ <keyhe.16@gmail.comkeyhe.16@gmail.com>
2017-12-04 00:08:53 -0600

    New Version

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,126 @@
-var container = document.getElementById('gameTable')
-var score = document.getElementById('score')
-var maxScore = document.getElementById('maxScore')
-var restart = document.getElementById('resetart')
-var imgs = document.getElementsByClassName('toys')
-var resetartBtn = document.getElementById('resetartBtn')
-resetartBtn.addEventListener('click', newGame)
-var pairs = 10
-var match = 0
-var toys = [
-  'img/artefacto_small.png',
-  'img/artefacto_small.png',
-  'img/astronauta_small.png',
-  'img/astronauta_small.png',
-  'img/bus_small.png',
-  'img/bus_small.png',
-  'img/muñeca_small.png',
-  'img/muñeca_small.png',
-  'img/oso_small.png',
-  'img/oso_small.png',
-  'img/peluche_small.png',
-  'img/peluche_small.png',
-  'img/perro_small.png',
-  'img/perro_small.png',
-  'img/robot_small.png',
-  'img/robot_small.png',
-  'img/soldado_small.png',
-  'img/soldado_small.png',
-  'img/triciclo_small.png',
-  'img/triciclo_small.png']
+const toyry = (function () {
+  let toys = [
+    'img/artefacto_small.png',
+    'img/artefacto_small.png',
+    'img/astronauta_small.png',
+    'img/astronauta_small.png',
+    'img/bus_small.png',
+    'img/bus_small.png',
+    'img/muñeca_small.png',
+    'img/muñeca_small.png',
+    'img/oso_small.png',
+    'img/oso_small.png',
+    'img/peluche_small.png',
+    'img/peluche_small.png',
+    'img/perro_small.png',
+    'img/perro_small.png',
+    'img/robot_small.png',
+    'img/robot_small.png',
+    'img/soldado_small.png',
+    'img/soldado_small.png',
+    'img/triciclo_small.png',
+    'img/triciclo_small.png'];
 
-function newGame () {
-  // Qué limpiar en las cartas?
-  score.innerText = 0
-}
+  let currentCard;
+  let previousCard;
+  let currentCardId = 0;
+  let previousCardId = -1;
+  let flippedCards = 0;
+  let score = 0;
+  let match = false;
+  let matchCards = 0;
 
-function shuffle (array) {
-  var currentIndex = array.length
-  var tempValue
-  var randomIndex
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-
-    tempValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = tempValue
+  function _randomDeck(cards) {
+    const randomCards = cards.sort(function () { return 0.5 - Math.random(); });
+    return randomCards;
   }
 
-  return array
-}
+  function _displayCard() {
+    if (flippedCards === 2) {
+      flippedCards++;
+    }
 
-var toysRandom = shuffle(toys)
+    if (!this.isClicked) {
+      const id = toys[this.index].replace('img/cards/', '').replace('.png', '');
+      const img = this.querySelector('img');
 
-Array.from(imgs).forEach(function (element, index) {
-  element.src = toysRandom[index]
-})
+      previousCardId = currentCardId;
+      currentCardId = id;
 
-console.log(toysRandom)
+      previousCard = currentCard;
+      currentCard = this;
 
-function initGame () {
+      _matchCard(id);
 
-}
+      if (match) {
+        const cards = [previousCard, currentCard];
+        cards.forEach((card) => {
+          card.removeEventListener('click', _displayCard);
+          card.classList.add('is-deleted');
+        });
+      }
 
-function endGame () {
+      img.src = toys[this.index];
+      _initialPosition(this);
+      this.isClicked = true;
+    }
+  }
 
-}
+  function _matchCard() {
+    if (previousCardId === currentCardId) {
+      match = true;
+      score += 100;
+      matchCards += 1;
+      _score();
+    } else {
+      match = false;      
+    }
+  }
 
-function restart () {
+  function _mensage() {
+    if (matchCards === 10) {
+      let winMsg = document.createElement('p');
+      const text = document.createTextNode('You WIN!!');
+      winMsg.appendChild(text);
+      winMsg.className = "win-msg";
+      document.getElementById('gameTable').appendChild(winMsg);
+    }
+  }
 
-}
+  function _score() {
+    document.getElementById('score').innerText = score;
+    _mensage();
+  }
 
-function match () {
 
-}
+  function _initialPosition(card) {
+    setTimeout(function () {
+      currentCardId = 0;
+      card.isClicked = false;
+      flippedCards--;
+      setTimeout(function () {
+        card.querySelector('img').src = '../img/back_small.png';
+      }, 500);
+    }, 2000);
+  }
+
+  function _flipCard(cards) {
+    cards.forEach(function (card, index) {
+      card.isClicked = false;
+      card.index = index;
+      card.addEventListener('click', _displayCard);
+    });
+  }
+
+  function init() {
+    const cards = document.querySelectorAll('.cards');
+    toys = _randomDeck(toys);
+    _flipCard(cards);
+  }
+
+  return {
+    init: init,
+  };
+})();
+
+toyry.init();
